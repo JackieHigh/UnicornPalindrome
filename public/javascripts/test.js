faba8c4aa4bf0f8fbe295d3bf1edb5bd9402ece1
JakubPluhar <jakub.pluhar@gmail.com>
2018-06-14 21:13:53 +0200

    Little bit of refactoring

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,42 +1,41 @@
 const palindrome = require('./Palindrome');
-
 QUnit.test("Negative palindrome test", function (assert) {
     var value = palindrome.palindrome("Abc");
     assert.equal(value, false, "Conot palindrome.");
-}),
-    QUnit.test("Negative palindrome test", function (assert) {
-        var value = palindrome.palindrome("Ahoj");
-        assert.equal(value, false, "Correct, input is not palindrome.");
-    }),
-    QUnit.test("Negative palindrome test", function (assert) {
-        var value = palindrome.palindrome("Baf");
-        assert.equal(value, false, "Correct, input is not palindrome.");
-    }),
-    QUnit.test("Negative palindrome test", function (assert) {
-        var value = palindrome.palindrome("Lek");
-        assert.equal(value, false, "Correct, input is not palindrome.");
-    }),
-    QUnit.test("Negative palindrome test", function (assert) {
-        var value = palindrome.palindrome("Palindrom");
-        assert.equal(value, false, "Correct, input is not palindrome.");
-    }),
-    QUnit.test("Positive palindrome test", function (assert) {
-        var value = palindrome.palindrome("Anna");
-        assert.equal(value, true, "Correct, input is palindrome.");
-    }),
-    QUnit.test("Positive palindrome test", function (assert) {
-        var value = palindrome.palindrome("1aba1");
-        assert.equal(value, true, "Correct, input is palindrome.");
-    }),
-    QUnit.test("Positive palindrome test", function (assert) {
-        var value = palindrome.palindrome("V elipse spí lev.");
-        assert.equal(value, true, "Correct, input is palindrome.");
-    }),
-    QUnit.test("Positive palindrome test", function (assert) {
-        var value = palindrome.palindrome("Kuna nese nanuk.");
-        assert.equal(value, true, "Correct, input is palindrome.");
-    }),
-    QUnit.test("Positive palindrome test", function (assert) {
-        var value = palindrome.palindrome("Ahoj #Karle# jak se mášameskajelrakjoha.");
-        assert.equal(value, true, "Correct, input is palindrome.");
-    });
+});
+QUnit.test("Negative palindrome test", function (assert) {
+    var value = palindrome.palindrome("Ahoj");
+    assert.equal(value, false, "Correct, input is not palindrome.");
+});
+QUnit.test("Negative palindrome test", function (assert) {
+    var value = palindrome.palindrome("Baf");
+    assert.equal(value, false, "Correct, input is not palindrome.");
+});
+QUnit.test("Negative palindrome test", function (assert) {
+    var value = palindrome.palindrome("Lek");
+    assert.equal(value, false, "Correct, input is not palindrome.");
+});
+QUnit.test("Negative palindrome test", function (assert) {
+    var value = palindrome.palindrome("Palindrom");
+    assert.equal(value, false, "Correct, input is not palindrome.");
+});
+QUnit.test("Positive palindrome test", function (assert) {
+    var value = palindrome.palindrome("Anna");
+    assert.equal(value, true, "Correct, input is palindrome.");
+});
+QUnit.test("Positive palindrome test", function (assert) {
+    var value = palindrome.palindrome("1aba1");
+    assert.equal(value, true, "Correct, input is palindrome.");
+});
+QUnit.test("Positive palindrome test", function (assert) {
+    var value = palindrome.palindrome("V elipse spí lev.");
+    assert.equal(value, true, "Correct, input is palindrome.");
+});
+QUnit.test("Positive palindrome test", function (assert) {
+    var value = palindrome.palindrome("Kuna nese nanuk.");
+    assert.equal(value, true, "Correct, input is palindrome.");
+});
+QUnit.test("Positive palindrome test", function (assert) {
+    var value = palindrome.palindrome("Ahoj #Karle# jak se mášameskajelrakjoha.");
+    assert.equal(value, true, "Correct, input is palindrome.");
+});
